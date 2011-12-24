6652177af05f08bdf644867bfda3c1115a0c26fd
James Coleman <jtc331@gmail.com>
2011-12-24 10:43:27 -0500

    Require libs.

--- lib/mail_alternatives_with_attachments.rb
+++ lib/mail_alternatives_with_attachments.rb
@@ -1,5 +1,6 @@
 require "mail_alternatives_with_attachments/version"
 
 module MailAlternativesWithAttachments
-  # Your code goes here...
+  require "lib/action_mailer_prepare_message"
+  require "lib/mail_message_alternative_types_with_attachment"
 end
