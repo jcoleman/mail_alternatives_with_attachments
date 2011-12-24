a383479a3b2374917099b8dc71118a05f392f13b
James Coleman <jtc331@gmail.com>
2011-12-24 11:21:28 -0500

    Yet another require fix.

--- lib/mail_alternatives_with_attachments.rb
+++ lib/mail_alternatives_with_attachments.rb
@@ -1,6 +1,6 @@
 require "mail_alternatives_with_attachments/version"
 
 module MailAlternativesWithAttachments
-  require File.expand_path("../action_mailer_prepare_message", __FILE__)
-  require File.expand_path("../mail_message_alternative_types_with_attachment", __FILE__)
+  require "mail_alternatives_with_attachments/action_mailer_prepare_message"
+  require "mail_alternatives_with_attachments/mail_message_alternative_types_with_attachment"
 end
