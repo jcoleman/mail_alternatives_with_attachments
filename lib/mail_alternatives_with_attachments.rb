88389e1b1724d9df9015802d7da594b5e2e97d38
James Coleman <jtc331@gmail.com>
2011-12-24 10:53:54 -0500

    Correct requires to be properly relative.

--- lib/mail_alternatives_with_attachments.rb
+++ lib/mail_alternatives_with_attachments.rb
@@ -1,6 +1,6 @@
 require "mail_alternatives_with_attachments/version"
 
 module MailAlternativesWithAttachments
-  require "lib/action_mailer_prepare_message"
-  require "lib/mail_message_alternative_types_with_attachment"
+  require File.expand_path("../lib/action_mailer_prepare_message", __FILE__)
+  require File.expand_path("../lib/mail_message_alternative_types_with_attachment", __FILE__)
 end
